08caa9a4e33e201566650336e20b4eba628998f3
NortonDeicide <nortondeicide@gmail.com>
2013-04-15 22:48:42 +0400

    Update ItemApple.java

--- src/ItemApple.java
+++ src/ItemApple.java
@@ -16,17 +16,18 @@ public class ItemApple extends ItemTool
 {
     private int weaponDamage;
     private final EnumToolMaterial toolMaterial;
-    private static Block[] blocksEffectiveAgainst = new Block[] {Block.planks, Block.bookShelf, Block.wood, Block.chest, Block.stoneDoubleSlab, Block.stoneSingleSlab, Block.pumpkin, Block.pumpkinLantern, Block.stone, Block.stoneBrick, Block.cobblestone, Block.cobblestoneMossy, Block.cobblestoneWall, Block.oreCoal, Block.oreDiamond, Block.oreEmerald, Block.oreGold, Block.oreIron, Block.oreLapis, Block.oreRedstone, Block.oreRedstoneGlowing, Block.dirt, Block.sand, Block.sandStone, Block.gravel, Block.netherBrick, Block.netherFence, Block.netherrack, Block.netherStalk, Block.glowStone, Block.glass, Block.obsidian, Block.grass};
-
+    public static final Block[] blocksEffectiveAgainst = new Block[] {Block.planks, Block.bookShelf, Block.wood, Block.chest, Block.stoneDoubleSlab, Block.stoneSingleSlab, Block.pumpkin, Block.pumpkinLantern, Block.cobblestone, Block.stoneDoubleSlab, Block.stoneSingleSlab, Block.stone, Block.sandStone, Block.cobblestoneMossy, Block.oreIron, Block.blockSteel, Block.oreCoal, Block.blockGold, Block.oreGold, Block.oreDiamond, Block.blockDiamond, Block.ice, Block.netherrack, Block.oreLapis, Block.blockLapis, Block.oreRedstone, Block.oreRedstoneGlowing, Block.rail, Block.railDetector, Block.railPowered};
+    
     public ItemApple(int par1, EnumToolMaterial par2EnumToolMaterial)
     {
-    	super(par1, 3, par2EnumToolMaterial, blocksEffectiveAgainst);
+        super(par1, 3, par2EnumToolMaterial, blocksEffectiveAgainst);
         this.toolMaterial = par2EnumToolMaterial;
         this.maxStackSize = 1;
         this.setMaxDamage(par2EnumToolMaterial.getMaxUses());
         this.setCreativeTab(CreativeTabs.tabCombat);
         this.weaponDamage = 40 + par2EnumToolMaterial.getDamageVsEntity();
     }
+    
 
     public int func_82803_g()
     {
@@ -39,7 +40,7 @@ public class ItemApple extends ItemTool
      */
     public float getStrVsBlock(ItemStack par1ItemStack, Block par2Block)
     {
-        return par2Block != null && (par2Block.blockMaterial == Material.wood || par2Block.blockMaterial == Material.plants || par2Block.blockMaterial == Material.vine) ? this.efficiencyOnProperMaterial : super.getStrVsBlock(par1ItemStack, par2Block);
+        return par2Block != null && (par2Block.blockMaterial == Material.wood || par2Block.blockMaterial == Material.plants || par2Block.blockMaterial == Material.vine || par2Block.blockMaterial == Material.iron || par2Block.blockMaterial == Material.anvil || par2Block.blockMaterial == Material.rock) ? this.efficiencyOnProperMaterial : super.getStrVsBlock(par1ItemStack, par2Block);
     }
 
     /**
@@ -108,8 +109,8 @@ public class ItemApple extends ItemTool
      */
     public boolean canHarvestBlock(Block par1Block)
     {
-        return par1Block.blockID == Block.web.blockID;
-    }
+    	return par1Block == Block.obsidian ? this.toolMaterial.getHarvestLevel() == 3 : (par1Block != Block.blockDiamond && par1Block != Block.oreDiamond ? (par1Block != Block.oreEmerald && par1Block != Block.blockEmerald ? (par1Block != Block.blockGold && par1Block != Block.oreGold ? (par1Block != Block.blockSteel && par1Block != Block.oreIron ? (par1Block != Block.blockLapis && par1Block != Block.oreLapis ? (par1Block != Block.oreRedstone && par1Block != Block.oreRedstoneGlowing ? (par1Block.blockMaterial == Material.rock ? true : (par1Block.blockMaterial == Material.iron ? true : par1Block.blockMaterial == Material.anvil)) : this.toolMaterial.getHarvestLevel() >= 2) : this.toolMaterial.getHarvestLevel() >= 1) : this.toolMaterial.getHarvestLevel() >= 1) : this.toolMaterial.getHarvestLevel() >= 2) : this.toolMaterial.getHarvestLevel() >= 2) : this.toolMaterial.getHarvestLevel() >= 2);
+        }
 
     /**
      * Return the enchantability factor of the item, most of the time is based on material.
@@ -118,7 +119,7 @@ public class ItemApple extends ItemTool
     {
         return this.toolMaterial.getEnchantability();
     }
-
+           
     /**
      * Return the name for this tool's material.
      */
